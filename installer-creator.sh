5ec3833d730ee22206bc7ced04176d4797b2589a
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-09-22 18:56:20 +0000

    Added base content for shell script.

---
+++ installer-creator.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# SYNOPSIS
+
+# DESCRIPTION
+
+# REQUIREMENTS
+# - wget
+# - p7zip
+# - Qt Installer Framework 3.0 or higher
+# - UPX (Optional)
+
+INSTALLER_NAME='dist/moorhuhn-bundle-installer.run'
+
