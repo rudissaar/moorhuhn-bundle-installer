03b2c500d61c025c76cba4e325f8fe4652595fda
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-10-03 02:12:45 +0000

    Prototyping, added IMPORT_MOORHUHNJAGD method + wget binary capturing.

--- installer-creator.sh
+++ installer-creator.sh
@@ -13,12 +13,15 @@
 # Name of the installer that will be generated. 
 INSTALLER_NAME='dist/moorhuhn-bundle-installer.run'
 
+# Manual binary fallbacks.
+WGET_FALLBACK=''
+
 # Variable that captures parent directory of current script, do not modify.
 RELATIVE_PATH=$(dirname ${0})
 
 # If sources.ini file is not found, then lets generate it by copying sample.
 if [[ ! -f "${RELATIVE_PATH}/sources.ini" ]]; then
-    cp "${RELATIVE_PATH}/sources.ini" "${RELATIVE_PATH}/sources.ini"
+    cp "${RELATIVE_PATH}/sources.ini.sample" "${RELATIVE_PATH}/sources.ini"
 fi
 
 # Function that reads sources.ini file and return values from it.
@@ -26,6 +29,36 @@ GET_VALUE_FROM_DOT_INIT () {
     echo $(awk -F '=' '/'${1}'/ {print $2}' "${RELATIVE_PATH}/sources.ini")
 }
 
-MOORHUHNJAGD_URL=$(GET_VALUE_FROM_DOT_INIT moorhuhnjagd_url)
-echo "${MOORHUHNJAGD_URL}"
+MOORHUHNJAGD_ZIP_URL=$(GET_VALUE_FROM_DOT_INIT moorhuhnjagd_zip_url)
+
+IMPORT_MOORHUHNJAGD () {
+    if [[ -z "${MOORHUHNJAGD_ZIP_URL}" ]]; then
+        echo '> Source for Moorhuhjagd zip archive is unspecified.'
+        echo '> Skipping.'
+        return
+    fi
+
+    MOORHUHNJAGD_ARCHIVE_PATH="${RELATIVE_PATH}/tmp/moorhuhnjagd.zip"
+
+    if [[ ! -f "${MOORHUHNJAGD_ARCHIVE_PATH}" ]]; then
+        "${WGET}" "${MOORHUHNJAGD_ZIP_URL}" -O "${MOORHUHNJAGD_ARCHIVE_PATH}"
+    fi
+}
+
+# Capturing wget command.
+which wget 1> /dev/null 2>&1
+
+if [[ "${?}" != '0' ]]; then
+    if [[ ! -z "${WGET_FALLBACK}" ]]; then
+        WGET="${WGET_FALLBACK}"
+    else
+        echo "> Unable to find wget from your environment's PATH variable."
+        echo '> Aborting.'
+        exit 1
+    fi
+else
+    WGET="$(which wget)"
+fi
+
+IMPORT_MOORHUHNJAGD
 
