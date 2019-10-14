4210d111e5c9e776fd5c5aa1fad3e839e0cb56f5
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2019-10-14 16:20:41 +0100

    Added base script for windows platform.

---
+++ installer-creator.ps1
@@ -0,0 +1,15 @@
+<#
+ .SYNOPSIS
+
+ .DESCRIPTION
+
+ .REQUIREMENTS
+  - Windows PowerShell 4.0 or higher.
+  - Qt Installer Framework 3.0 or higher
+  - 7-Zip
+  - UPX (Optional)
+#>
+
+Set-StrictMode -Version 3
+
+$global:InstallerName = "${PSScriptRoot}\dist\moorhuhn-bundle-installer.exe"
